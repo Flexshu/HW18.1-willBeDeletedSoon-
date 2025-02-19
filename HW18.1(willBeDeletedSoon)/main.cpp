5c829c8ba2d699d9bea3231427c80042be176eb8
Flexshu <egor.wm202@gmail.com>
2025-02-19 18:43:42 +0200

    V2

--- HW18.1(willBeDeletedSoon)/main.cpp
+++ HW18.1(willBeDeletedSoon)/main.cpp
@@ -9,5 +9,14 @@ int main() {
         {"Boleyko", 2008, "speaker", 75000.75, "politician"},
         {"Zadorozhna", 209, "chef", 100, "compote maker"}};
     
+    int counter = 0;
+    for (int i=0; i<5; i++) {
+        if (2025 - staff[i].birthYear >= 60) {
+            counter++;
+            staff[i].printInfo();
+        }
+    }
+    cout<<"There are "<<counter<<" workers oldar than 60 years in total.\n";
+    
     return 0;
 }
